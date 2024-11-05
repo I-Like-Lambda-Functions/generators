2ace64b1c0ad0f5bce6d3c3a41eedabaf317e989
I-Like-Lambda-Functions <173729232+I-Like-Lambda-Functions@users.noreply.github.com>
2024-11-05 14:52:49 +0100

    Updated password_gen.py
    -optimized list comprehension;
    -added tests;

--- password_gen.py
+++ password_gen.py
@@ -1,13 +1,22 @@
-from random import choice, shuffle
+from random import choice, shuffle, randint
 
 # default ASCII
 def password_generator(num_of_lower: int = 1, num_of_capital: int = 1, num_of_spec_chars: int = 1, include_non_printable: bool = False, num_of_digits: int = 1, random_order: bool = True) -> str:
-    lower = [chr(choice(range(97, 123))) for _ in range(num_of_lower)]
-    capital = [chr(choice(range(65, 91))) for _ in range(num_of_capital)]
+    spec_chars_list = list(
+        (set(range(32, 48)) |
+         set(range(58, 65)) |
+         set(range(91, 97)) |
+         set(range(123, 127))) ^
+        (set(range(32), 127) if include_non_printable else set())
+    )
+    lower = [chr(randint(97, 122)) for _ in range(num_of_lower)]
+    capital = [chr(randint(65, 90)) for _ in range(num_of_capital)]
     # Python pozwala na operacje logiczne między zbiorami zbiorami (setami).
-    special = [chr(choice(list((set(range(32, 48)) | set(range(58, 65)) | set(range(91, 97)) | set(range(123, 127))) ^ (set(range(32), 127) if include_non_printable else set())))) for char in range(num_of_spec_chars)]
-    digits = [chr(choice(range(48, 58))) for _ in range(num_of_digits)]
+    special = [chr(choice(spec_chars_list)) for _ in range(num_of_spec_chars)]
+    digits = [chr(randint(48, 57)) for _ in range(num_of_digits)]
     password = lower + capital + special + digits
-    if random_order:
-        shuffle(password)
+    if random_order: shuffle(password)
     return ''.join(password)
+
+assert len(password_generator(4, 4, 4, False, 4, True)) == 16
+assert len(password_generator(1, 1, 2, False, 10, False)) == 14
